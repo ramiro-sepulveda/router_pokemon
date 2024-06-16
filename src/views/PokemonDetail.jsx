20aa81619e0096145f058c31429b70096bb27118
ramiro-sepulveda <162350790+ramiro-sepulveda@users.noreply.github.com>
2024-06-16 05:31:57 -0400

    excepcion nlist useEffect

--- src/views/PokemonDetail.jsx
+++ src/views/PokemonDetail.jsx
@@ -14,6 +14,7 @@ const PokemonDetail = () => {
 
   useEffect(() => {
     consultarApi();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const consultarApi = async () => {
